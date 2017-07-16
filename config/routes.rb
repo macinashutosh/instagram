17b29580d72cafc10af6ef49fe542a7b479c2b78
garry329 <jaideep329@gmail.com>
2017-07-16 19:35:39 +0530

    added home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index' 
+  root 'home#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :posts
    post 'likes/like'
