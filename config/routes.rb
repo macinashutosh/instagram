0de059a2d5c8a501bc1bc1166b7f9cce7606ea3c
garry329 <jaideep329@gmail.com>
2017-07-16 22:21:36 +0530

    added profile

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :posts
    post 'likes/like'
+   get '/profile/index' => 'profile#index'
    post 'comments/comment'
 
   # The priority is based upon order of creation: first created -> highest priority.
