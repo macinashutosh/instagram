2223de55723bd81f20d902ee187b7970d70ff1a7
macinashutosh <itssmartashutosh@gmail.com>
2017-08-02 22:17:07 +0530

    validation of image added

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ class Post < ActiveRecord::Base
   has_many :likes
   has_many :comments
     has_attached_file :image, styles: { medium: "600x600#", thumb: "190x190#" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image  , content_type: /\Aimage\/.*\z/
+   validates_attachment_content_type :image  , content_type: /\Aimage\/.*\z/
+   validates_presence_of :image
 end
